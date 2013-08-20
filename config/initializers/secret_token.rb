633f52974d04c808054321a41a0d366361e8935d
Debora Sujono <dsujono@fiksu.com>
2013-08-20 03:00:12 -0400

    Created Housemates app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Housemates::Application.config.secret_token = 'b0b0482f041d74b03c2550edecdab9da0054b07255ede3e5240eca475c13e3d4a31745d905761561d58acb2b655787cf619df4434ea7bb08f34cd983cda0756d'
