03d4440cdd4e9251f4e8aba1ccfdd2a2ff36d12a
Debora Sujono <deborausujono@gmail.com>
2013-09-12 00:24:13 -0400

    Fixed Heroku deployment error (asset precompiling)

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Housemates
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent initializing the application and connecting to the database while precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
