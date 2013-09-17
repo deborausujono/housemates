e6d430d5165c2241a2adb1a330aa12ba8a0fff97
Debora Sujono <deborausujono@gmail.com>
2013-09-17 16:14:33 -0400

    Bug fixes

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -15,18 +15,17 @@ $(document).ready(function() {
 
   //Edit and delete links
   $('#editable-list li.note-cell').
-    on('mousedown', '#edit-note-container form #cancel_button', function() {
+    on('mousedown', '#edit-note-container form #cancel-edit-button', function() {
       $(this).parent().attr('data-remote', 'true');
     }).
-    on('click', '#edit-note-container form #cancel_button', function() {
+    on('click', '#edit-note-container form #cancel-edit-button', function() {
       $(this).parent().parent().attr('id', 'current-edit-note-container');
     }).
     on('click', '#note-container #edit-note-link', function() {
       $(this).parent().parent().parent().attr('id', 'current-note-container');
     }).
     on('mousedown', '#note-container #delete-link', function() {
-      var href = $(this).parent().parent().find('#confirm-delete #delete-button').attr('href');
-      $(this).parent().parent().find('#confirm-delete #delete-button').attr('href', href.replace('path', $(this).data('path')));
+      $('#confirm-delete #delete-button').attr('href', $(this).data('path'));
   });
 });
 
