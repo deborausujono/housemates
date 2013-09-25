c88fb5722be0d2a986aea74a1b07851139cfcd30
Debora Sujono <deborausujono@gmail.com>
2013-09-25 09:59:04 -0400

    Updated Houses#index, Houses#show, and Welcome#index UI

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   //Show options (edit and delete) on hover
 	$('#editable-list li.note-cell .thumbnail #option-links').hide();
-  $('#editable-list li.note-cell .thumbnail #option-links-filler').show();
+  $('#editable-list li.note-cell .thumbnail #house-option-links').hide();
 
 	$('#editable-list li.note-cell').hover(
     function () {
       $(this).find('.thumbnail #option-links').show();
-      $(this).find('.thumbnail #option-links-filler').hide();
+      $(this).find('.thumbnail #house-option-links').show();
   },
     function () {
       $(this).find('.thumbnail #option-links').hide();
-      $(this).find('.thumbnail #option-links-filler').show();
+      $(this).find('.thumbnail #house-option-links').hide();
   });
 
   //Edit and delete links
@@ -22,14 +22,16 @@ $(document).ready(function() {
       $(this).parent().parent().attr('id', 'current-edit-note-container');
     }).
     on('click', '#note-container #edit-note-link', function() {
-      $(this).parent().parent().parent().attr('id', 'current-note-container');
+      $(this).parent().parent().parent().parent().parent().attr('id', 'current-note-container');
     }).
     on('mousedown', '#note-container #delete-link', function() {
       $('#confirm-delete #delete-button').attr('href', $(this).data('path'));
+    }).
+    on('mousedown', '#delete-house-link', function() {
+      $('#confirm-delete-house #delete-button').attr('href', $(this).data('path'));
   });
 });
 
 $( document ).ajaxComplete(function() {
   $('#editable-list li.note-cell .thumbnail #option-links').hide();
-  $('#editable-list li.note-cell .thumbnail #option-links-filler').show();
 });
